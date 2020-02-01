4b411139734b636bb6ebda0b42ec79e6c2903d55
Brian Salemi <briandsalemi@gmail.com>
2020-02-01 16:31:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 let mainNode = document.querySelector('#main')
 mainNode.remove()
 
-let newHeader = document.querySelector("h1 #victory");
+let newHeader = document.querySelector("h1#victory");
 newHeader.innerHTML = "YOUR-NAME is the champion"
